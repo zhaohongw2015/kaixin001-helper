c2c006a8c9565d2e7c1930fbbdaa86b703975d5c
yangyonggan2008 <yangyonggan2008@09dd445e-d021-11dd-8ca8-e3ff79f713b6>
2009-04-13 14:04:47 +0000

    20090414 1、修改账号操作中“置顶”“置底”的错误 2、统计功能中增加“车辆总价值”信息 3、增加查找好友的功能

---
+++ trunk/SNSHelper_Win_Parker/ParkSta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using SNSHelper.Kaixin001;
+using SNSHelper.Kaixin001.Entity.Parking;
+using SNSHelper_Win_Garden.Entity;
+using SNSHelper_Win_Garden.Helper;
+
+namespace SNSHelper_Win_Garden
+{
+    public partial class frmMain
+    {
+        #region 账号统计
+        private void parkerSta(ParkerStaInfo parkerStainfo)
+        {
+            Boolean bExits = false;
+            foreach (DataGridViewRow dr in parkStaGridViewX1.Rows)
+            {
+                if (dr.Cells["UserId"].Value.ToString().Equals(parkerStainfo.UId.ToString()))
+                {
+                    dr.Cells["carcount"].Value = parkerStainfo.Carcount;
+                    dr.Cells["CarPriceCount"].Value  = parkerStainfo.CarCountPrice;
+                    dr.Cells["cash"].Value = parkerStainfo.Cashcount;
+                    dr.Cells["TotalProperty"].Value = parkerStainfo.Cashcount + parkerStainfo.CarCountPrice;
+                    bExits = true;
+                }
+
+            }
+            if (!bExits)
+            {
+                parkStaGridViewX1.Rows.Add(parkerStainfo.RealName, parkerStainfo.UId, parkerStainfo.Carcount, parkerStainfo.CarCountPrice, parkerStainfo.Cashcount, (parkerStainfo.Cashcount + parkerStainfo.CarCountPrice));
+
+            }
+
+
+        }
+
+        #endregion
+
+    }
+   
+}
